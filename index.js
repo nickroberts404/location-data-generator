cc5b42e7f14eac0075662c1ee5a20316d22531cf
nickroberts404 <nickroberts404@gmail.com>
2017-01-23 00:06:14 -0600

    Laid out a very basic koa webserver

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var bodyParser = require('koa-bodyparser');
+var router = require('koa-router')();
+var logger = require('koa-logger');
+var koa = require('koa');
+var app = koa();
+var port = 3030;
+
+app.use(logger());
+
+app.use(bodyParser());
+
+router.get('/', function *(next) {
+	console.log('We got a request!');
+});
+
+app.use(router.routes())
+app.use(router.allowedMethods());
+
+app.listen(port);
+console.log('Now listening at http://localhost:'+port);
