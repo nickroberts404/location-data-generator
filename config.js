505320f7e5e8a3617f331e3df391feb86cc695ad
nickroberts404 <nickroberts404@gmail.com>
2017-01-23 18:55:36 -0600

    Added bounding box argument to commander, along with basic parsing.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
 	port: 3030,
 	nodeCount: 5,
+	bbox: [[90, 180],[-90, -180]],
 }
--- options.js
+++ options.js
@@ -4,9 +4,16 @@
 var program = require('commander');
 var config = require('./config');
 
+var parseBBox = (i) => {
+	i = i.split(',').map(parseFloat);
+	if(i.length !== 4) return config.bbox;
+	return [[i[0], i[1]], [i[2], i[3]]];
+}
+
 program
 	.version('0.0.1')
 	.option('-n, --nodes <n>', 'The amount of nodes', i => parseInt(i), config.nodeCount)
+	.option('-b, --bbox <n>', 'The bounding box', parseBBox, config.bbox)
 	.parse(process.argv);
 
 module.exports = program;
