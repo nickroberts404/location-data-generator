5baf8e4b59f87a4509ffb762e5ef9714702ebcbc
nickroberts404 <nickroberts404@gmail.com>
2017-03-01 13:08:56 -0600

    Began splitting up tests, need to find way to build them

---
+++ test/test.locations.js
@@ -0,0 +1,26 @@
+describe('locations.js', () => {
+	describe('randomPoints()', () => {
+		it('Returns 1000', () => {
+			const result = locations.randomPoints();
+			expect(result).to.equal(1000);
+		})
+	})
+	describe('getPointsInPolygon()', () => {
+		it('Returns 1000', () => {
+			const result = locations.getPointsInPolygon();
+			expect(result).to.equal(1000);
+		})
+	})
+	describe('getPointsInBBox()', () => {
+		it('Returns 1000', () => {
+			const result = locations.getPointsInBBox();
+			expect(result).to.equal(1000);
+		})
+	})
+	describe('randomInRange()', () => {
+		it('Returns 1000', () => {
+			const result = locations.randomInRange();
+			expect(result).to.equal(1000);
+		})
+	})
+})
